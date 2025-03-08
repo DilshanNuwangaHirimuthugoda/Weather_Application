0433ad9850ff5870de0928b208e2e3452ea62371
DilshanNuwangaHirimuthugoda <dilshannuwangahirimuthugoda@gmail.com>
2025-03-08 23:47:39 +0530

    Add event listener for search button and initialize API key

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const search = document.querySelector('.search-box button');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
+
+search.addEventListener('click', () => {
+
+    const APIKey = "981e96c96aaec017e4457ae21a92e646";
+    const city = document.querySelector('.search-box input').value;
